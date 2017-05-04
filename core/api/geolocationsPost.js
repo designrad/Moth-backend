2820baaff1af39393505356ad4211d9d17854d30
Perervenko Vladislav <vperervenko@allmax.team>
2017-05-04 17:23:57 +0700

    change json format for geolocations

--- core/api/geolocationsPost.js
+++ core/api/geolocationsPost.js
@@ -1,56 +1,70 @@
 'use strict';
 
 let async = require('asyncawait/async'),
-  await = require('asyncawait/await'),
-  jsonFile = require('jsonfile'),
-  moment = require('moment'),
-  fs = require('fs');
+    await = require('asyncawait/await'),
+    jsonFile = require('jsonfile'),
+    moment = require('moment'),
+    fs = require('fs');
 
 let model = require('../db/model'),
-  API = require('../APILib'),
-  path = require('../path'),
-  utils = require('../utils'),
-  CONST = require('../constants');
+    API = require('../APILib'),
+    path = require('../path'),
+    utils = require('../utils'),
+    CONST = require('../constants');
 
 module.exports = async((req, res) => {
-  let dataSession = req.session.data;
+    let dataSession = req.session.data;
 
-  if (dataSession && dataSession.session && dataSession.userId) {
-      let session = await(model.Session.findOne({session: dataSession.session}).exec());
-      if (!session || session && session.admin != dataSession.userId) {
-          return API.fail(res, API.errors.UNAUTHORIZED);
-      }
-  } else {
-    return API.fail(res, API.errors.UNAUTHORIZED)
-  }
-
-  const images = await(model.Photo.find().exec()),
-    fileName = CONST.filesName.geolocations;
-
-  if (fs.existsSync(path.PUBLIC.GEOLOCATIONS + `/${fileName}`)) {
-    await(fs.unlink(path.PUBLIC.GEOLOCATIONS + `/${fileName}`));
-  }
-
-  let geolocationsJson = {};
-  for (let i = 0; i < images.length; i++) {
-    const image = images[i];
-    geolocationsJson[image.name] = {
-      id: image._id,
-      name: image.name,
-      comments: image.comments,
-      latitude: image.latitude,
-      longitude: image.longitude,
-      identification: image.identification,
-      date: moment(image.date).format("DD.MM.YYYYY HH:mm:ss")
+    if (dataSession && dataSession.session && dataSession.userId) {
+        let session = await(model.Session.findOne({session: dataSession.session}).exec());
+        if (!session || session && session.admin != dataSession.userId) {
+            return API.fail(res, API.errors.UNAUTHORIZED);
+        }
+    } else {
+        return API.fail(res, API.errors.UNAUTHORIZED)
     }
-  }
 
-  await(jsonFile.writeFile(path.PUBLIC.GEOLOCATIONS + `/${fileName}`, geolocationsJson, function (err) {
-    console.error(err);
-    if (err) return API.fail(res, err);
-  }));
+    const images = await(model.Photo.find().exec()),
+        fileName = CONST.filesName.geolocations;
 
-  return API.success(res, {
-    fileName
-  });
+    if (fs.existsSync(path.PUBLIC.GEOLOCATIONS + `/${fileName}`)) {
+        await(fs.unlink(path.PUBLIC.GEOLOCATIONS + `/${fileName}`));
+    }
+
+    let features = [];
+    for (let i = 0; i < images.length; i++) {
+        const image = images[i];
+        features.push({
+            "type": "Feature",
+            "properties": {
+                "marker-color": "#7E7E7E",
+                "marker-size": "medium",
+                "marker-symbol": "",
+                "id": image._id,
+                "name": image.name,
+                "comments": image.comments,
+                "identification": image.identification,
+                "date": moment(image.date).format("DD.MM.YYYYY HH:mm:ss")
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    image.latitude,
+                    image.longitude
+                ]
+            }
+        });
+    }
+
+    await(jsonFile.writeFile(path.PUBLIC.GEOLOCATIONS + `/${fileName}`, {
+        type: "FeatureCollection",
+        "features": features
+    }, function (err) {
+        console.error(err);
+        if (err) return API.fail(res, err);
+    }));
+
+    return API.success(res, {
+        fileName
+    });
 });
